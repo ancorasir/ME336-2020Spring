e78daa4c4d3012f1826d3cb987f945382d3d5339
Gesheng <11612122@mail.sustc.edu.cn>
2020-04-04 18:52:11 +0800

    add tic tac toe cv version

--- Simulation/tasks/Tic_tac_toe/main_cv.py
+++ Simulation/tasks/Tic_tac_toe/main_cv.py
@@ -24,7 +24,57 @@ def chessboard_bias_position(action):
                         [[0,-1,0],  [0,0,0],  [0,1,0]],
                         [[1,-1,0],  [1,0,0],  [1,1,0]]]).reshape((-1,3))[action]
     return a
-    
+
+def cv_get_position(img,depth_image):
+    '''
+    para
+    ---
+        img: np.ndarray type image
+    return
+    ---
+        tuple of O chesses, X chesses and chessboard
+        example: ([pos1, pos2, ...],[pos1, pos2, ...],pos)
+    help
+    ---
+    area
+        chess: ~=350
+        chessboard: ~= 27700
+    color
+        X chess: blue HSV:[120 255 255]
+        O chess: red HSV:[0 255 255]
+        backgroud: yellow BRG:[0 255 255] HSV:[30 255 255] HSV_range:([20 200 200],[40 255 255])
+    '''
+
+    # create binay image
+    upper_yellow = np.array([40, 255, 255])
+    lower_yellow = np.array([20, 200, 200])
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    bin = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    bin = cv2.bitwise_not(bin)
+    # find contours
+    contours, _ = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(img, contours, -1, (0,255,0), 3)
+    X_chesses = []
+    O_chesses = []
+    for cnt in contours:
+        # filte by area
+        area = cv2.contourArea(cnt)
+        M = cv2.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        if area < 400:# chess
+            # filte by color
+            color = img[cy,cx]
+            hsv = cv2.cvtColor(np.array([[color]]), cv2.COLOR_BGR2HSV)
+            if cv2.inRange(hsv, np.array([100,200,200]), np.array([140,255,255]))[0][0]==255:# blue X
+                X_chesses.append((cam.H@cam.uv2XYZ(depth_image,cx,cy))[0:3])
+            else: # red O
+                O_chesses.append((cam.H@cam.uv2XYZ(depth_image,cx,cy))[0:3])
+        elif 20000 < area and area < 30000: # chessboard
+            chessboard = cam.H@cam.uv2XYZ(depth_image,cx,cy)
+    return O_chesses, X_chesses, chessboard[0:3]
+
 def move_chess_pipeline(chess_pos,chessboard_pos,action):
     # get the pos to put
     put_pos = np.array(chessboard_pos) + chessboard_bias_position(action)
@@ -32,8 +82,11 @@ def move_chess_pipeline(chess_pos,chessboard_pos,action):
     franka.clear_path = True
     chess_pos[2] += 0.1
     franka.move(env,chess_pos,euler=[0,np.radians(180),0])
-    chess_pos[2] -= 0.1
+    chess_pos[2] -= 0.11
     franka.move(env,chess_pos,euler=[0,np.radians(180),0])
+    for chess in chesses:
+        if franka.gripper._proximity_sensor.is_detected(chess):
+            break
     franka.grasp(env,chess)
     chess_pos[2] += 0.1
     franka.move(env,chess_pos,euler=[0,np.radians(180),0])
@@ -47,7 +100,21 @@ def move_chess_pipeline(chess_pos,chessboard_pos,action):
     #put_pos[2] +=0.09
     #franka.move(env,put_pos,euler=[0,np.radians(180),0])
     franka.home(env)
+'''
+img = cv2.imread('img.png')
+env = Env(scene('Tic_tac_toe.ttt'))
+env.start()
+cam = Camera()
 
+depth_image = np.load('depth_img.npy')
+O_chesses, X_chesses, chessboard = cv_get_position(img,depth_image)
+print(chessboard)
+
+env.stop()
+env.shutdown()
+cv2.imshow('img',img)
+cv2.waitKey()
+'''
 if __name__ == "__main__":
     env = Env(scene('Tic_tac_toe.ttt'))
     env.start()
@@ -59,15 +126,17 @@ if __name__ == "__main__":
 
     # cam 
     cam = Camera()
-    imgs = []
+    img = cam.capture_bgr()
+    depth_img = cam.capture_depth(in_meters=True)
+    O_chesses_pos, X_chesses_pos, chessboard_pos = cv_get_position(img,depth_img)
+    
     # chessboard and chesses
-    chessboard = Shape('chessboard')
-    chesses = {'X':[Shape('chess_X'+str(chess_num)) for chess_num in range(1,6)],
-                'O':[Shape('chess_O'+str(chess_num)) for chess_num in range(1,6)]}
-
+    chesses_pos = {'X':X_chesses_pos,'O':O_chesses_pos}
+    chesses = [Shape('chess_X'+str(chess_num)) for chess_num in range(1,6)]+\
+              [Shape('chess_O'+str(chess_num)) for chess_num in range(1,6)]
     # craete chessboard instance
     board = Board()
-    imgs.append(cam.capture_bgr())
+    
     # Decide who first (X always first)
     system('clear')
     instream = input('Do you want to play first hand?[y/n]')
@@ -86,6 +155,7 @@ if __name__ == "__main__":
     Ai_chess_count = 0
     player_chess_count = 0
     while True:
+        
         system('clear')
         print(f'You = {player_chess}\tAi = {Ai_chess}\n')
         board.print_board()
@@ -134,10 +204,9 @@ if __name__ == "__main__":
             board._move(action,player_chess)
 
             # get player chess
-            chess = chesses[player_chess][player_chess_count]
+            chess = chesses_pos[player_chess][player_chess_count]
             player_chess_count += 1
-            move_chess_pipeline(chess.get_position(),chessboard.get_position(),action)
-
+            move_chess_pipeline(chess,chessboard_pos,action)
             turn = "AI"
 
         elif turn == "AI":
@@ -146,10 +215,9 @@ if __name__ == "__main__":
             board._move(action,Ai_chess)
             # get AI chess
             
-            chess = chesses[Ai_chess][Ai_chess_count]
+            chess = chesses_pos[Ai_chess][Ai_chess_count]
             Ai_chess_count += 1
-            move_chess_pipeline(chess.get_position(),chessboard.get_position(),action)
-
+            move_chess_pipeline(chess,chessboard_pos,action)
             turn = 'player'
     input('Game over[Press Enter to quit]')
     env.stop()
